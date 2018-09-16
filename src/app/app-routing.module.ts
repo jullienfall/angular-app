4e589860532c74a82ef5300e6510f4885d62732c
jullienfall <jullien.fall@gmail.com>
2018-09-16 13:45:48 +0300

    routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+
+const routes: Routes = [
+  { path: 'blog', component: BlogComponent },
+  { path: '', redirectTo: '/blog', pathMatch: 'full' },
+  { path: 'contact', component: ContactComponent }
+]
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    ContactComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
