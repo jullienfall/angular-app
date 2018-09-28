c2f9321bca63e66921de6a84de2bc70c8f326305
Julia Kyryk <jullien.fall@gmail.com>
2018-09-28 17:24:47 +0300

    slider bootstrap

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent {
+  public slides: Array<Object> = [
+    {
+      video1: 'https://mdbootstrap.com/img/video/Lines.mp4',
+      video2: 'https://mdbootstrap.com/img/video/Tropical.mp4',
+      video3: 'https://mdbootstrap.com/img/video/animation-intro.mp4'
+    },
+    {
+      video1: 'https://mdbootstrap.com/img/video/forest.mp4',
+      video2: 'https://mdbootstrap.com/img/video/Tropical.mp4',
+      video3: 'https://mdbootstrap.com/img/video/animation-intro.mp4'
+    },
+    {
+      video1: 'https://mdbootstrap.com/img/video/Lines.mp4',
+      video2: 'https://mdbootstrap.com/img/video/forest.mp4',
+      video3: 'https://mdbootstrap.com/img/video/animation-intro.mp4'
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  playPause(video) {
+    if (video.paused) {
+      video.play();
+    } else video.pause();
   }
-
 }
