bcc2417a51f971fff3a3a2ce8221ee82b98eec91
Julia Kyryk <jullien.fall@gmail.com>
2018-10-01 15:30:12 +0300

    videogular

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -8,25 +8,19 @@ import { Component } from '@angular/core';
 export class SliderComponent {
   public slides: Array<Object> = [
     {
-      video1: 'https://mdbootstrap.com/img/video/Lines.mp4',
-      video2: 'https://mdbootstrap.com/img/video/Tropical.mp4',
-      video3: 'https://mdbootstrap.com/img/video/animation-intro.mp4'
+      video1: 'http://static.videogular.com/assets/videos/videogular.mp4',
+      video2: 'https://mdbootstrap.com/img/video/forest.mp4',
+      video3: 'https://mdbootstrap.com/img/video/Lines.mp4'
     },
     {
       video1: 'https://mdbootstrap.com/img/video/forest.mp4',
-      video2: 'https://mdbootstrap.com/img/video/Tropical.mp4',
+      video2: 'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
       video3: 'https://mdbootstrap.com/img/video/animation-intro.mp4'
     },
     {
       video1: 'https://mdbootstrap.com/img/video/Lines.mp4',
-      video2: 'https://mdbootstrap.com/img/video/forest.mp4',
+      video2: 'http://static.videogular.com/assets/videos/elephants-dream.mp4',
       video3: 'https://mdbootstrap.com/img/video/animation-intro.mp4'
     }
   ];
-
-  playPause(video) {
-    if (video.paused) {
-      video.play();
-    } else video.pause();
-  }
 }
--- src/app/slider/slider.module.ts
+++ src/app/slider/slider.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
+import { VgBufferingModule } from 'videogular2/buffering';
 
-import {SliderComponent} from './slider.component';
- 
+import { SliderComponent } from './slider.component';
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, VgCoreModule, VgControlsModule, VgOverlayPlayModule, VgBufferingModule],
   declarations: [SliderComponent]
 })
-export class SliderModule { }
+export class SliderModule {}
