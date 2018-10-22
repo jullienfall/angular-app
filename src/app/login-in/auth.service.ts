7afbc1f3e3fa5b9c63ec1049917b80fa615c8c27
Julia Kyryk <jullien.fall@gmail.com>
2018-10-22 15:20:27 +0300

    localStorage

--- src/app/login-in/auth.service.ts
+++ src/app/login-in/auth.service.ts
@@ -12,18 +12,35 @@ export class AuthService {
   constructor() {}
 
   isLoggedIn(): boolean {
+    if (localStorage.getItem('currentUser')) {
+      let localUser = JSON.parse(localStorage.getItem('currentUser'));
+      this.currentUser = {
+        id: localUser.id,
+        userName: localUser.userName,
+        password: localUser.password
+      };
+    }
     return !!this.currentUser;
   }
 
   login(userName: string, password: string): void {
     this.currentUser = {
-      id: 2,
+      id: 1,
       userName: userName,
-      isAdmin: false
+      password: password
     };
+    localStorage.setItem(
+      'currentUser',
+      JSON.stringify({
+        id: this.currentUser.id,
+        userName: this.currentUser.userName,
+        password: this.currentUser.password
+      })
+    );
   }
 
   logout(): void {
     this.currentUser = null;
+    localStorage.removeItem('currentUser');
   }
 }
